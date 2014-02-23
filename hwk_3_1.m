1711a260dd51ac657bdb7c320e6cce048bfeb6d9
EmilyEinsig <emccathr@gmu.edu>
2014-02-23 10:33:47 -0500

    Update hwk_3_1.m

--- hwk_3_1.m
+++ hwk_3_1.m
@@ -1,4 +1,3 @@
-
 clear; clc;
 
 for r=1:1000
@@ -17,13 +16,6 @@ for r=1:1000
     s_ub_avg(r) = mean(s_ub);
 end
 
-% Calculate bin size
-A = sort(s_b_avg);
-qtr = A(size(s_b_avg,2)/4);
-size_bin = 2*qtr/(size(s_b_avg,2)^(1/3));
-n_bin = (max(s_b_avg)-min(s_b_avg))/size_bin;
-% Did not do this correctly; got n_bin ~1, but not sure what is wrong.
-
 % Plot histograms of average standard deviations
 figure(1);
 hist(s_b_avg,25);
@@ -36,6 +28,6 @@ title('Comparison of Biased(Blue) and Ubiased(Red) Standard Deviation');
 
 hold off;
 
-% Calculate significance value with p-test
-z = (mean(s_b_avg)-mean(s_ub_avg))/(std(s_ub_avg)/sqrt(size(s_b_avg,2)));
-p = 1/(2*pi)*exp((-1*z^2)/2))
+% Calculate significance value with z-test
+z = (mean(s_b_avg)-mean(s_ub_avg))/(std(s_ub_avg)/sqrt(size(s_b_avg,2)))
+p = 1/(2*pi)*exp((-1*z^2)/2)
