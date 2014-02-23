f0c5f0df552581a0284eff0d34c1b44aaacb72df
EmilyEinsig <emccathr@gmu.edu>
2014-02-23 14:13:31 -0500

    Update hwk_3_3.m

--- hwk_3_3.m
+++ hwk_3_3.m
@@ -33,7 +33,9 @@ fprintf('Statistic D = %4.5f\n\n',statD);
     
 %Compute (s_D)^2
 
-s_D2 = 1/(size(y1,1)+size(y2,1)-2)*((size(y2,1)*y2_std^2)+(size(y1,1)*y1_std^2))
+s_D2 = 1/(size(y1,1)+size(y2,1)-2)*((size(y2,1)*y2_std^2)+(size(y1,1)*y1_std^2));
+
+fprintf('S_D^2 = %4.5f\n\n',s_D2);
 
 % Repeat Steps a-e 1000 times
 for m=1:1000
@@ -58,15 +60,25 @@ for m=1:1000
     statD(m) = y2_m-y1_m;
 end
 
-num_gt2 = length(find(statD>2.0))
+num_gt2 = length(find(statD>2.0));
+
+fprintf('Number of values >2.0 = %i\n\n',num_gt2);
 
 
 % Calculate area under curve for standard normal distribution where x > 2.0
-syms x;
+% erf(x) = 2/sqrt(pi) * integral from 0 to x of exp(-t^2) dt.
+% To get the integral from 2 to infinity, erf(inf)-erf(2)
 
-phi_prob = int(1/sqrt(2*pi)*exp(-x^2/2),2,inf); %Not sure how to get rid of the ugliness here when displayed
+phi_prob = 1/(2*sqrt(2*pi))*(erf(inf)-erf(sqrt(2)));
 
-prob_d_gt2 = (100/2)*phi_prob
+fprintf('Probability of one value being >2.0 = %4.5f\n\n',phi_prob);
+
+% Use Bernoulli equation to determine the probability of N/2 >2.0
+
+
+prob_d_gt2 = factorial(100)/(factorial(50)*factorial(100-50))*phi_prob^(50)*(1-phi_prob)^(50);
+
+fprintf('Probability of half of the values being >2.0 = %4.5f\n\n',prob_d_gt2);
 
 
     
