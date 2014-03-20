ba6f09f93ed37f4ddd1e3cb9744b072b9c70bee3
EmilyEinsig <emccathr@gmu.edu>
2014-03-20 15:34:34 -0400

    Update hwk_6_3.m

--- hwk_6_3.m
+++ hwk_6_3.m
@@ -2,60 +2,47 @@ clear; clc;
 
 % m = 10
 % Create time series using fiter function
-x10 = randn(10, 1);
-y10 = filter(1, [1 -0.6 0.08 0.2 0.1 -0.04 1 0.05 0.5 -0.3], x10);
+u = randn(40, 1);
+z10 = filter(1, u, [1 -0.6 0.08 0.2 0.1 -0.04 1 0.05 0.5 -0.3]);
 
-% Create A matrix for slash operation
-A10 = zeros(10);
+z10=z10';
 
+% Create matrix A from u values for slash operation
 for i=1:10
-    A10(i,i) = x10(i);
+    for j=1:10
+        k = 10-i+1+j-1;
+        A_10(j,i) = u(k);
+    end
 end
 
-b_10 = A10\y10
+b_10 = A_10\z10
 
-figure('Name','m = 10');
-plot(x10,y10,'r');
-hold on;
-plot(A10*b_10,y10,'b');
-legend('Original','Calculated');
+% Using same u, determine what b is for m=3
+z3 = filter(1, u, [0.6 0.2 -0.04]);
 
-% m = 3
-% Create time series using fiter function
-x3 = randn(3, 1);
-y3 = filter(1, [0.08 0.2 -0.04], x3);
+z3=z3';
 
-% Create A matrix for slash operation
-A3 = zeros(3);
-
+% Create matrix A from u values for slash operation
 for i=1:3
-    A3(i,i) = x3(i);
+    for j=1:3
+        k = 3-i+1+j-1;
+        A_3(j,i) = u(k);
+    end
 end
 
-b_3 = A3\y3
+b_3 = A_3\z3
 
-figure('Name','m = 3');
-plot(x3,y3,'r');
-hold on;
-plot(A3*b_3,y3,'b');
-legend('Original','Calculated');
+% Using same u, determine what b is for m=12
+z12 = filter(1, u, [0.05 -0.08 0.6 0.7 1 -0.2 0.3 0.6 0.4 0.4 0.2 -0.04]);
 
-% m = 12
-% Create time series using fiter function
-x12 = randn(12, 1);
-y12 = filter(1, [1 -0.6 0.08 0.2 0.1 -0.04 1 0.05 0.5 -0.3 -0.5 0.4], x12);
+z12=z12';
 
-% Create A matrix for slash operation
-A12 = zeros(12);
-
+% Create matrix A from u values for slash operation
 for i=1:12
-    A12(i,i) = x12(i);
+    for j=1:12
+        k = 12-i+1+j-1;
+        A_12(j,i) = u(k);
+    end
 end
 
-b_12 = A12\y12
-
-figure('Name','m = 12');
-plot(x12,y12,'r');
-hold on;
-plot(A12*b_12,y12,'b');
-legend('Original','Calculated');
+b_12 = A_12\z12
