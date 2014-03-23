2357882d5f5c36a1061bc63686a7e9066b0a9c01
EmilyEinsig <emccathr@gmu.edu>
2014-03-23 16:39:02 -0400

    Update hwk_6_4.m

--- hwk_6_4.m
+++ hwk_6_4.m
@@ -3,9 +3,9 @@ clear; clc;
 load('OMNI_OMNI2_merged_20130927-v1.mat');
 
 % Calculate tau values from data
-for i=2:size(Plasma_bulk_speed,1)
+for i=1:size(Plasma_bulk_speed,1)-1
     E(i) = Plasma_bulk_speed(i)*(abs(Bz_GSM(i))-Bz_GSM(i))/2;
-    tau(i-1) = Dst_index(i)/(E(i) - Dst_index(i) + Dst_index(i-1));
+    tau(i) = Dst_index(i)/(E(i) - Dst_index(i+1) + Dst_index(i));
 end
 
 tau=tau';
@@ -22,3 +22,5 @@ figure();
 hist(tau,60);
 xlabel('Tau Value');
 ylabel('Number of values');
+
+tau_avg = mean(tau)
