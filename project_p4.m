563d6a5e9ddc19b55717426eb27edf3b3548f1e3
EmilyEinsig <emccathr@gmu.edu>
2014-04-14 20:37:12 -0400

    proj_p4

--- project_p4.m
+++ project_p4.m
@@ -86,111 +86,183 @@ ft_p_5 = 1/Fs*[0:1/NFFT5:1/2];
 
 % Zero out peaks
 % stat1
- ft_1_z = ft_1;
+ft_1_z = ft_1;
 [max_value11, index11] = max(2*abs(ft_1(2:NFFT1/2+1)));
-ft_1_z(index11:index11+2)=0;
+index11 = index11+1;
+ft_1_z(index11-6:index11+12)=mean(ft_1(index11+13:index11+33));
+ft_1_z(end-index11-12:end)=mean(ft_1(end-index11-33:end-index11-13)); % need to mirror zeroing
 [max_value21, index21] = max(2*abs(ft_1(10:NFFT1/2+1)));
-ft_1_z(index21:index21+2)=0;
+index21 = index21+9;
+ft_1_z(index21-10:index21+12)=1/2*(abs(mean(ft_1(index21-19:index21-9)))+abs(mean(ft_1(index21+13:index21+23))));
+ft_1_z(end-index21-10:end-index21+12)=1/2*(abs(mean(ft_1(end-index21-19:end-index21-9)))+abs(mean(ft_1(end-index21+13:end-index21+23)))); % need to mirror zeroing
+ 
+% stat2
+ft_2_z = ft_2;
+[max_value12, index12] = max(2*abs(ft_2(2:NFFT2/2+1)));
+index12 = index12+1;
+ft_2_z(index12-6:index12+12)=mean(ft_2(index12+13:index12+33));
+ft_2_z(end-index12-12:end)=mean(ft_2(end-index12-33:end-index12-13)); % need to mirror zeroing
+[max_value22, index22] = max(2*abs(ft_2(10:NFFT2/2+1)));
+index22 = index22+9;
+ft_2_z(index22-10:index22+12)=1/2*(abs(mean(ft_2(index22-19:index22-9)))+abs(mean(ft_2(index22+13:index22+23))));
+ft_2_z(end-index22-10:end-index22+12)=1/2*(abs(mean(ft_2(end-index22-19:end-index22-9)))+abs(mean(ft_2(end-index22+13:end-index22+23)))); % need to mirror zeroing
+ 
+% stat3
+ft_3_z = ft_3;
+[max_value13, index13] = max(2*abs(ft_3(2:NFFT3/2+1)));
+index13 = index13+1;
+ft_3_z(index13-6:index13+12)=mean(ft_3(index13+13:index13+33));
+ft_3_z(end-index13-12:end)=mean(ft_3(end-index13-33:end-index13-13)); % need to mirror zeroing
+[max_value23, index23] = max(2*abs(ft_3(10:NFFT3/2+1)));
+index23 = index23+9;
+ft_3_z(index23-10:index23+14)=1/2*(abs(mean(ft_3(index23-19:index23-9)))+abs(mean(ft_3(index23+13:index23+23))));
+ft_3_z(end-index23-10:end-index23+12)=1/2*(abs(mean(ft_3(end-index23-19:end-index23-9)))+abs(mean(ft_3(end-index23+13:end-index23+23)))); % need to mirror zeroing
 
+% stat4
+ft_4_z = ft_4;
+[max_value14, index14] = max(2*abs(ft_4(2:NFFT4/2+1)));
+index14 = index14+1;
+ft_4_z(index14-6:index14+12)=mean(ft_4(index14+13:index14+33));
+ft_4_z(end-index14-12:end)=mean(ft_4(end-index14-33:end-index14-13)); % need to mirror zeroing
+[max_value24, index24] = max(2*abs(ft_4(10:NFFT4/2+1)));
+index24 = index24+9;
+ft_4_z(index24-10:index24+12)=1/2*(abs(mean(ft_4(index24-19:index24-9)))+abs(mean(ft_4(index24+13:index24+23))));
+ft_4_z(end-index24-10:end-index24+12)=1/2*(abs(mean(ft_4(end-index24-19:end-index24-9)))+abs(mean(ft_4(end-index24+13:end-index24+23)))); % need to mirror zeroing
+ 
+% stat5
+ft_5_z = ft_5;
+[max_value15, index15] = max(2*abs(ft_5(2:NFFT5/2+1)));
+index15 = index15+1;
+ft_5_z(index15-6:index15+12)=mean(ft_5(index15+13:index15+33));
+ft_5_z(end-index15-12:end)=mean(ft_5(end-index15-33:end-index15-13)); % need to mirror zeroing
+[max_value25, index25] = max(2*abs(ft_5(10:NFFT5/2+1)));
+index25 = index25+9;
+ft_5_z(index25-10:index25+12)=1/2*(abs(mean(ft_5(index25-19:index25-9)))+abs(mean(ft_5(index25+13:index25+23))));
+ft_5_z(end-index25-10:end-index25+12)=1/2*(abs(mean(ft_5(end-index25-19:end-index25-9)))+abs(mean(ft_5(end-index25+13:end-index25+23)))); % need to mirror zeroing
 
+% Plot zeroed FFTs
 figure(1);
-subplot(2,1,1);
-plot(ft_p_1(2:end),2*abs(ft_1(2:NFFT1/2+1))) 
+subplot(5,1,1);
+plot(ft_p_1(2:end),2*abs(ft_1(2:NFFT1/2+1)),'b') 
 hold on;
-plot([ft_p_1(index21+1),ft_p_1(index21+1)],[0,0.6],'k-');
-title('Single-Sided Amplitude Spectrum of B(t) - Stationary 1')
-xlabel('Frequency (Hz)')
-ylabel('|B(f)|')
-hold off;
-subplot(2,1,2);
-plot(ft_p_1(2:end),2*abs(ft_1_z(2:NFFT1/2+1)))
-title('Single-Sided Amplitude Spectrum of Zeroed B(t) - Stationary 1')
+plot(ft_p_1(2:end),2*abs(ft_1_z(2:NFFT1/2+1)),'r')
+title('Stationary 1')
 xlabel('Frequency (Hz)')
 ylabel('|B(f)|')
 axis([0 35 0 0.6]);
+legend('Original FFT','Zeroed FFT');
+hold off;
 
- 
-%  % stat2
-%  ft_2_z = 2*abs(ft_2(2:NFFT2/2+1));
-% [max_value12, index12] = max(ft_2_z(1:end));
-% ft_2_z(index12-1:index12+1)=0;
-% [max_value22, index22] = max(ft_2_z(10:end));
-% ft_2_z(index22-1:index22+1)=0;
-%  
-%  % stat3
-%  ft_3_z = ft_3;
-% [max_value13, index13] = max(2*abs(ft_3_z(2:NFFT3/2+1)));
-% ft_3_z(index13:index13+2)=0;
-% [max_value23, index23] = max(2*abs(ft_3_z(10:NFFT3/2+1)));
-% ft_3_z(index23:index23+2)=0;
-%  
-%  % stat4
-%  ft_4_z = ft_4;
-% [max_value14, index14] = max(2*abs(ft_4_z(2:NFFT4/2+1)));
-% ft_4_z(index14:index14+2)=0;
-% [max_value24, index24] = max(2*abs(ft_4_z(10:NFFT4/2+1)));
-% ft_4_z(index24:index24+2)=0;
-%  
-%  % stat5
-%  ft_5_z = ft_5;
-% [max_value15, index15] = max(2*abs(ft_5_z(2:NFFT5/2+1)));
-% ft_5_z(index15:index15+2)=0;
-% [max_value25, index25] = max(2*abs(ft_5_z(10:NFFT5/2+1)));
-% ft_5_z(index25:index25+2)=0;
+subplot(5,1,2);
+plot(ft_p_2(2:end),2*abs(ft_2(2:NFFT2/2+1)),'b') 
+hold on;
+plot(ft_p_2(2:end),2*abs(ft_2_z(2:NFFT2/2+1)),'r')
+title('Stationary 2')
+xlabel('Frequency (Hz)')
+ylabel('|B(f)|')
+axis([0 35 0 0.6]);
+hold off;
+
+subplot(5,1,3);
+plot(ft_p_3(2:end),2*abs(ft_3(2:NFFT3/2+1)),'b') 
+hold on;
+plot(ft_p_3(2:end),2*abs(ft_3_z(2:NFFT3/2+1)),'r')
+title('Stationary 3')
+xlabel('Frequency (Hz)')
+ylabel('|B(f)|')
+axis([0 35 0 0.6]);
+hold off;
+
+subplot(5,1,4);
+plot(ft_p_4(2:end),2*abs(ft_4(2:NFFT4/2+1)),'b') 
+hold on;
+plot(ft_p_4(2:end),2*abs(ft_4_z(2:NFFT4/2+1)),'r')
+title('Stationary 4')
+xlabel('Frequency (Hz)')
+ylabel('|B(f)|')
+axis([0 35 0 0.6]);
+hold off;
+
+subplot(5,1,5);
+plot(ft_p_5(2:end),2*abs(ft_5(2:NFFT5/2+1)),'b') 
+hold on;
+plot(ft_p_5(2:end),2*abs(ft_5_z(2:NFFT5/2+1)),'r')
+title('Stationary 5')
+xlabel('Frequency (Hz)')
+ylabel('|B(f)|')
+axis([0 35 0 0.6]);
+hold off;
+
 
 
 % Calculate inverse FFT
-% Bi_1_z = ifft(ft_1_z,NFFT1)*L1+mean(Bi_1);
-% Bi_2_z = ifft(ft_2_z,NFFT2)*L2+mean(Bi_2);
-% Bi_3_z = ifft(ft_3_z,NFFT3)*L3+mean(Bi_3);
-% Bi_4_z = ifft(ft_4_z,NFFT4)*L4+mean(Bi_4);
-% Bi_5_z = ifft(ft_5_z,NFFT5)*L5+mean(Bi_5);
+Bi_1_z = ifft(ft_1_z,NFFT1)*L1+mean(Bi_1);
+Bi_2_z = ifft(ft_2_z,NFFT2)*L2+mean(Bi_2);
+Bi_3_z = ifft(ft_3_z,NFFT3)*L3+mean(Bi_3);
+Bi_4_z = ifft(ft_4_z,NFFT4)*L4+mean(Bi_4);
+Bi_5_z = ifft(ft_5_z,NFFT5)*L5+mean(Bi_5);
 
 % Plot results of inverse FFT
 
 % stat1
-% figure(1);
-% subplot(5,1,1);
-% plot(ti_1,abs(Bi_1_z(1:size(ti_1,2))));
-% datetick('x','HH:MM:SS');
-% xlabel('Time');
-% ylabel('B Magnitude (nT)');
-% title('Inverse FFT on B Magnitude - Stationary 1');
-% 
-% % stat2
-% subplot(5,1,2);
-% plot(ti_2,abs(Bi_2_z(1:size(ti_2,2))));
-% datetick('x','HH:MM:SS');
-% xlabel('Time');
-% ylabel('B Magnitude (nT)');
-% title('Inverse FFT on B Magnitude - Stationary 2');
-% 
-% % stat3
-% subplot(5,1,3);
-% plot(ti_3,abs(Bi_3_z(1:size(ti_3,2))));
-% datetick('x','HH:MM:SS');
-% xlabel('Time');
-% ylabel('BMagnitude (nT)');
-% title('Inverse FFT on B Magnitude - Stationary 3');
-% 
-% % stat4
-% subplot(5,1,4);
-% plot(ti_4,abs(Bi_4_z(1:size(ti_4,2))));
-% datetick('x','HH:MM:SS');
-% xlabel('Time');
-% ylabel('B Magnitude (nT)');
-% title('Inverse FFT on B Magnitude - Stationary 4');
-% 
-% % stat5
-% subplot(5,1,5);
-% plot(ti_5,abs(Bi_5_z(1:size(ti_5,2))));
-% datetick('x','HH:MM:SS');
-% xlabel('Time');
-% ylabel('B Magnitude (nT)');
-% title('Inverse FFT on B Magnitude - Stationary 5');
+figure(2);
+subplot(5,1,1);
+plot(ti_1/(1000000*3600*24),abs(Bi_1(1:size(ti_1,2))),'b');
+hold on;
+plot(ti_1/(1000000*3600*24),abs(Bi_1_z(1:size(ti_1,2))),'r');
+datetick('x','HH:MM:SS');
+xlabel('Time');
+ylabel('B Magnitude (nT)');
+title('Inverse FFT on B Magnitude - Stationary 1');
+legend('Original','Zeroed');
+hold off;
+
+% stat2
+subplot(5,1,2);
+plot(ti_2/(1000000*3600*24),abs(Bi_2(1:size(ti_2,2))),'b');
+hold on;
+plot(ti_2/(1000000*3600*24),abs(Bi_2_z(1:size(ti_2,2))),'r');
+datetick('x','HH:MM:SS');
+xlabel('Time');
+ylabel('B Magnitude (nT)');
+title('Inverse FFT on B Magnitude - Stationary 2');
+hold off;
+
+% stat3
+subplot(5,1,3);
+plot(ti_3/(1000000*3600*24),abs(Bi_3(1:size(ti_3,2))),'b');
+hold on;
+plot(ti_3/(1000000*3600*24),abs(Bi_3_z(1:size(ti_3,2))),'r');
+datetick('x','HH:MM:SS');
+xlabel('Time');
+ylabel('B Magnitude (nT)');
+title('Inverse FFT on B Magnitude - Stationary 3');
+hold off;
+
+% stat4
+subplot(5,1,4);
+plot(ti_4/(1000000*3600*24),abs(Bi_4(1:size(ti_4,2))),'b');
+hold on;
+plot(ti_4/(1000000*3600*24),abs(Bi_4_z(1:size(ti_4,2))),'r');
+datetick('x','HH:MM:SS');
+xlabel('Time');
+ylabel('B Magnitude (nT)');
+title('Inverse FFT on B Magnitude - Stationary 4');
+hold off;
+
+% stat5
+subplot(5,1,5);
+plot(ti_5/(1000000*3600*24),abs(Bi_5(1:size(ti_5,2))),'b');
+hold on;
+plot(ti_5/(1000000*3600*24),abs(Bi_5_z(1:size(ti_5,2))),'r');
+datetick('x','HH:MM:SS');
+xlabel('Time');
+ylabel('B Magnitude (nT)');
+title('Inverse FFT on B Magnitude - Stationary 5');
+hold off;
 
 
-% CONDUCT FFT ON MOVING WINDOW OF INVERSED B MAGNITUDE
+% CONDUCT FFT ON MOVING WINDOW OF INVERSED ZEROED B MAGNITUDE
 % stat1
 % Fs = (ti(2)-ti(1))*10^-6;% Sampling frequency, convert from microseconds to seconds
 % L1 = size(Bi_1,2); % Length of signal
